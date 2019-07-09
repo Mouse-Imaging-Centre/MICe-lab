629b542719efab776e416e894dd702035a3937a6
nzxwang <nickywang100@gmail.com>
2019-07-09 14:07:05 -0400

    version bump and add fastai to dependency_links

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import sys
 
 setup(name="MICe-lab",
-      version='0.18',
+      version='0.19',
       description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.',
       long_description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.',
       url='https://github.com/Mouse-Imaging-Centre/MICe-lab',
@@ -23,6 +23,7 @@ setup(name="MICe-lab",
       ],
       dependency_links=['https://github.com/opencv/opencv/archive/3.1.0.tar.gz',
                         'https://github.com/Mouse-Imaging-Centre/pydpiper',
+                        'https://github.com/fastai/fastai',
                         'https://github.com/Mouse-Imaging-Centre/fastcell'],
       packages=find_packages(),
       scripts=['saddle_reconstruction/saddle_reconstruct.py',
